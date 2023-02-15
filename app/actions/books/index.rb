cb36411b57819aaa69a2d0fbf9cc3af060fcfafc
Osnyder12 <snyderowen12@gmail.com>
2023-02-15 08:34:48 -0500

    ended at Showing a book

--- app/actions/books/index.rb
+++ app/actions/books/index.rb
@@ -6,10 +6,21 @@ module Bookshelf
       class Index < Bookshelf::Action
         include Deps["persistence.rom"]
 
-        def handle(*, response)
+        params do
+          optional(:page).value(:integer, gt?: 0)
+          # page is optional and must be greater than 0
+          optional(:per_page).value(:integer, gt?: 0, lteq?: 100)
+          # per page is optional must be greater than 0 and less than or equal to 100 
+        end
+
+        def handle(request, response)
+          halt 422, {errors: request.params.errors}.to_json unless request.params.valid?
+
           books = rom.relations[:books]
             .select(:title, :author)
             .order(:title)
+            .page(request.params[:page] || 1)
+            .per_page(request.params[:per_page] || 5)
             .to_a
 
           response.format = :json
