c16303fb2c9316d9dacb1daa145b871a64dd9dee
Osnyder12 <snyderowen12@gmail.com>
2023-02-16 11:16:48 -0500

    ended on Creating a book

--- app/action.rb
+++ app/action.rb
@@ -5,5 +5,16 @@ require "hanami/action"
 
 module Bookshelf
   class Action < Hanami::Action
+    # .one! method (in actions/books/show.rb) raises ROM::TupleCountMismatchError if no record is found
+    # call the this will catch this exception and call the handle_not_found method
+    config.handle_exception ROM::TupleCountMismatchError => :handle_not_found
+
+    private
+
+    def handle_not_found(_request, response, _exception)
+      response.status = 404
+      response.format = :json
+      response.body = {error: "not_found"}.to_json
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 module Bookshelf
   class Routes < Hanami::Routes
-    root to: "home.show"
+    root to: "home.index"
     get "/books", to: "books.index"
+    get "/books/:id", to: "books.show"
   end
 end
