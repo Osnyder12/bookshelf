dda5e22a6ca2a3877dc8b3c545b7ed984aaa0808
Osnyder12 <snyderowen12@gmail.com>
2023-02-16 11:30:43 -0500

    finished Introduction

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@
 
 module Bookshelf
   class Routes < Hanami::Routes
-    root to: "home.index"
+    root to: "home.show"
     get "/books", to: "books.index"
     get "/books/:id", to: "books.show"
+    post "/books", to: "books.create"
   end
 end
